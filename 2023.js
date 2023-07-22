2a7394adf7098305921c743aac2963c42ca16982
kaithub <140258324+kaithub@users.noreply.github.com>
2023-07-22 16:41:10 +0200

    fhgfj

--- 2023.js
+++ 2023.js
@@ -1,11 +1,11 @@
 {
-  "faction: "/r/placeautism",
+  "faction": "/r/placeautism",
   "contact": "https://discord.gg/MbNB4CzV9h or nonkainary#4587",
   "templates": [
     {
       "name": "Pride Heart w/ Border",
       "sources": [
-        "https://cdn.discordapp.com/attachments/1132305087620988969/1132320959307722772/heartwborder.png"
+        "https://github.com/r-placeautism/templates/blob/main/heartwborder.png?raw=true"
       ],
       "x": -820,
       "y": 340
