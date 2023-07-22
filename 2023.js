0f99dfa5b7ab8eec17273c36092620693044ea3b
kaithub <140258324+kaithub@users.noreply.github.com>
2023-07-22 16:33:10 +0200

    Create 2023.js

---
+++ 2023.js
@@ -0,0 +1,25 @@
+{
+  "faction: "/r/placeautism",
+  "contact": "https://discord.gg/MbNB4CzV9h or nonkainary#4587",
+  "templates": [
+    {
+      "name": "Pride Heart w/ Border",
+      "sources": [
+        "https://github.com/r-placeautism/templates/blob/main/heartwborder.png?raw=true"
+      ],
+      "x": -820,
+      "y": 340
+    }
+  ],
+  "whitelist": [
+    {
+      "name": "r/PlacePride",
+      "url": "https://pride.place/template.json"
+    },
+    {
+      "name": "Trans Place",
+      "url": "https://trans.gg/template.json"
+    }
+  ],
+  "blacklist": []
+}
